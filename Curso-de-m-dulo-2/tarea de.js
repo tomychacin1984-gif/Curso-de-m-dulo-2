6f9900a70bc44f3768bce5034205f96801a914e0
tomychacin1984-gif <tomychacin1984@gmail.com>
2025-08-02 14:03:40 -0400

    Add files via upload

---
+++ Curso-de-m-dulo-2/tarea de.js
@@ -0,0 +1,100 @@
+let nombre = Cars
+let edad = 23 
+let estudiante  = true 
+   
+consola.log("nombre, nombre)");
+consola.log("edad, edad)");
+consola.log("estudiante", estudiante);
+
+//ejercicio 2 
+
+let nombreusuario = prompt("cual es su nombre");
+let añordenacimiento = numbe("en que año nacio usted");
+let ununnacimiento = numbe("nacimiento");
+let edadaproxi = 2025-fecha.aproxi;
+
+consola.log(hola,"+ nombreusuario +",tienes mas o menos" + edadaproxi + años.");
+
+//ejercicio 3
+
+let numeri1 = Number(promot("ingresa el primer numero numero");
+let numer2 = Number(promot("ingresa el segundo numero"));
+
+consola.log("",);
+consola.log("",);
+consola.log("")
+
+
+//ejercicio 1 de bucle
+
+let años = prompt("ingrese su edad")
+ 
+if (edad> 20) {
+    consola.log("es mayor de edad;"); 
+} else{
+    consola.log("es menor de edad");
+}
+   //comparar numeros 
+
+   let numero1 = parsefloat("ingresa el primer numero"));
+   let numero2 = parsefloat("ingres el primer numero"));
+
+   //
+
+   
+   ejercicio switch 1 y 2
+   
+   
+   
+   let diasemana = parsalnt (prompt("ingresa numero del 1 al 7"));
+
+switch (diasemana)¨{
+    case 1:
+    console.log ("lunes");
+    break;
+    
+    case 2:
+    console.log("martes");
+    break;
+    
+    case 3:
+    console.log("miercoles");
+    break; 
+     
+    case 4:
+    console.log("jueves");
+    break;
+
+    case 5:
+    console.log("viernes");
+    break;
+
+    case 6:
+    console.log("sabado");
+    break;
+
+    case 7:
+    console.log("domingo");
+    break;
+    
+    //ejercicio 2
+    
+    
+let opcionmenu= prompt("muñeco puede sentarse a jugar,despues a comer")
+
+switch (opcionmenu) {
+    case "muñeco":
+        console.log("preparate para la accion y emocion.");
+        break; // 'break' es crucial para no seguir ejecutando los siguientes casos.
+    case "jugar":
+        console.log("claro progresamos a judar a muchos retos.");
+        break;
+    case "comer":
+        console.log("despues de termina la aventura comer bien y calmado.");
+        break;
+    default: // Se ejecuta si ninguno de los casos anteriores coincide.
+        console.log("que intentas hacer bobo.");
+        break;
+
+
+  
